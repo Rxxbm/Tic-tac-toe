35c21cde92929d91fdab40cbfdc7ea7cbb9a25aa
Rxxbm <rubemcorrea0@gmail.com>
2023-01-13 00:49:30 -0200

    versão inicial

--- assets/js/index.js
+++ assets/js/index.js
@@ -28,23 +28,41 @@ div.forEach((btn) => {
       }
     });
     if (
+      (playsX.includes(0) && playsX.includes(1) && playsX.includes(2)) ||
+      (playsX.includes(3) && playsX.includes(4) && playsX.includes(5)) ||
+      (playsX.includes(6) && playsX.includes(7) && playsX.includes(8))
+    ) {
+        alert('Ganhou na horizontal')
+    } else if (
+      (playsX.includes(0) && playsX.includes(3) && playsX.includes(6)) ||
+      (playsX.includes(1) && playsX.includes(4) && playsX.includes(7)) ||
+      (playsX.includes(2) && playsX.includes(5) && playsX.includes(8))
+    ) {
+        alert("Venceu na vertical");
+    } else if (
+      (playsX.includes(0) && playsX.includes(4) && playsX.includes(8)) ||
+      (playsX.includes(2) && playsX.includes(4) && playsX.includes(6))
+    ) {
+        alert('Ganhou na diagonal')
+    }
+    console.log(playsX);
+    if (
       (playsO.includes(0) && playsO.includes(1) && playsO.includes(2)) ||
       (playsO.includes(3) && playsO.includes(4) && playsO.includes(5)) ||
       (playsO.includes(6) && playsO.includes(7) && playsO.includes(8))
     ) {
-        alert('jhkhj')
+      alert("Ganhou na horizontal");
     } else if (
       (playsO.includes(0) && playsO.includes(3) && playsO.includes(6)) ||
       (playsO.includes(1) && playsO.includes(4) && playsO.includes(7)) ||
       (playsO.includes(2) && playsO.includes(5) && playsO.includes(8))
     ) {
-        alert("jhkwwhj");
+      alert("Venceu na vertical");
     } else if (
       (playsO.includes(0) && playsO.includes(4) && playsO.includes(8)) ||
-      (playsO.includes(6) && playsO.includes(4) && playsO.includes(5))
+      (playsO.includes(2) && playsO.includes(4) && playsO.includes(6))
     ) {
-        alert('Ganhou na diagonal')
+      alert("Ganhou na diagonal");
     }
-    console.log(playsX);
   });
 });
